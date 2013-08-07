bbbfe358dc2763b065f66d20eb3bd26a35f167e2
Bunlong <bunlong.van@gmail.com>
2013-08-07 08:48:54 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrettyPageTitle::Application.config.secret_token = '5f604813de98f81c4605a57bbd300f33e30662322d5ab52420f5686a2105db35a9950ae24d95e0a914f87f8912553d53d7f4f7d7b41a772838e84abf9865d9d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PrettyPageTitle::Application.config.session_store :cookie_store, key: '_pretty_page_title_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PrettyPageTitle::Application.config.session_store :active_record_store
